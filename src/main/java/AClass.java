f71558e2a8966266da81e30dc517da73ca07fbae
Vika <viktoria.bozhko@jetbrains.com>
2021-12-01 11:37:32 +0300

    Commit files **1**

--- src/main/java/AClass.java
+++ src/main/java/AClass.java
@@ -2,7 +2,7 @@ public class AClass {
 
     public static void main(String[] args) {
         for (int i = 0; i < 50; i++) {
-            System.out.println("Print " + i);
+            System.out.println("Print: " + i);
         }
     }
 }
